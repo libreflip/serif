33d88f4c89b507bc5f1a1ec7ad7a2f4ecb8a2a60
Lux <lux@lux.name>
2019-04-19 00:18:22 +0200

    fix scroll with JS

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,4 +1,13 @@
 import Controller from '@ember/controller';
+import Hammer from 'hammerjs';
+
+const hammertime = new Hammer(document.body, {});
+function scroll(ev) {
+	document.body.scrollTop += (ev.deltaX / 10);
+}
+hammertime.on('panleft', scroll);
+hammertime.on('panright', scroll);
+
 
 export default Controller.extend({
   step: '',
