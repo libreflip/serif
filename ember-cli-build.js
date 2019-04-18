c6a50196c204e78d4995a82c7b67c6a4c5918ae2
Lux <lux@lux.name>
2019-04-19 00:04:03 +0200

    some basics wih css rotation

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,24 +1,19 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const MergeTrees = require('broccoli-merge-trees');
+const Funnel = require('broccoli-funnel');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+  });
+  app.import('node_modules/normalize.css/normalize.css');
+  app.import('node_modules/@typopro/web-source-sans-pro/TypoPRO-SourceSansPro.css');
+
+  const sourceSansPro = new Funnel('node_modules/@typopro/web-source-sans-pro', {
+    destDir: 'assets',
+    include: ['*.eot', '*.ttf', '*.woff'],
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-
-  return app.toTree();
+  return new MergeTrees([sourceSansPro, app.toTree()]);
 };
