78ca13b61172422c6f9f5309794f8c9baabc5427
Dunkhan <drunken4ever@gmail.com>
2019-07-27 18:41:33 +0200

    Implemented some unit tests. Removed 'in' component

--- tests/acceptance/tutorial-test.js
+++ tests/acceptance/tutorial-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, find } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | tutorial', function(hooks) {
@@ -7,13 +7,15 @@ module('Acceptance | tutorial', function(hooks) {
 
 	test('should have next button.', async function (assert){
 		await visit ('/tutorial');
-		assert.equal(find('button'), true)
+		assert.ok(find('button'))
 	})
 	test('should show some kind of graphic.', async function (assert){
 		await visit ('/tutorial');
-		assert.equal(find('.test'), true)
+		assert.ok(find('.tutorial-image'))
 	})
 	test('should show some text.', async function (assert){
+		await visit ('/tutorial');
+		assert.ok(find('.tutorial-text'))
 	})
 
 	test('visiting /tutorial', async function(assert) {
