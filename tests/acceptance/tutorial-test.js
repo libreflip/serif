f009902c3fb3691e833e91df92639361e6a15376
Dunkhan <drunken4ever@gmail.com>
2019-07-27 17:35:18 +0200

    implemented one test case

--- tests/acceptance/tutorial-test.js
+++ tests/acceptance/tutorial-test.js
@@ -3,18 +3,22 @@ import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | tutorial', function(hooks) {
-  setupApplicationTest(hooks);
+	setupApplicationTest(hooks);
 
-  test('should have next button.', async function (assert){
-  })
-  test('should show some kind of graphic.', async function (assert){
-  })
-  test('should show some text.', async function (assert){
-  })
+	test('should have next button.', async function (assert){
+		await visit ('/tutorial');
+		assert.equal(find('button'), true)
+	})
+	test('should show some kind of graphic.', async function (assert){
+		await visit ('/tutorial');
+		assert.equal(find('.test'), true)
+	})
+	test('should show some text.', async function (assert){
+	})
 
-  test('visiting /tutorial', async function(assert) {
-    await visit('/tutorial');
+	test('visiting /tutorial', async function(assert) {
+		await visit('/tutorial');
 
-    assert.equal(currentURL(), '/tutorial');
-  });
+		assert.equal(currentURL(), '/tutorial');
+	});
 });
